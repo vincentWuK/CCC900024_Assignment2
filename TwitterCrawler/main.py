798e8d020b1f2885844f4b359af7b69721369348
kuizong <a1752656@student.adelaide.edu.au>
2022-05-12 22:12:11 +0930

    update main

--- TwitterCrawler/main.py
+++ TwitterCrawler/main.py
@@ -14,34 +14,50 @@ RANK = comm.Get_rank()  # get the current process's rank
 SIZE = comm.Get_size()  # get the number of processes
 
 
-def gettweetsstream(topic, city, conf, account_num, is_suburb):
+def gettweetsstream(topic, conf, account_num, is_suburb):
+    if is_suburb:
+        while (True):
+            gettweetsuburb(topic, "melbourne", conf, account_num)
+    else:
+        while (True):
+            gettweetcity(topic, conf, account_num)
+
+
+def gettweetcity(topic, conf, account_num):
     apikey = conf["auth"][str(account_num)]["apikey"]
     apisecret = conf["auth"][str(account_num)]["apisecret"]
     accesstoken = conf["auth"][str(account_num)]["accesstoken"]
     accesssecret = conf["auth"][str(account_num)]["accesssecret"]
     bearer_token = conf["auth"][str(account_num)]["bearer_token"]
-    if is_suburb:
-        suburbs = conf["suburb"]
-        for suburb in suburbs:
-            name = suburb + ", " + city
-            try:
-                bbox = FindBBox(name)
-            except BaseException as e:
-                print("cannot find bbox: ", e)
-            worker = TweetsCrawler(apikey, apisecret, accesstoken,
-                                   accesssecret, bearer_token,
-                                   conf["keywordsfile"][topic],
-                                   [city, suburb, bbox], conf["server"],
-                                   conf["database"][topic])
-            worker.GetTweetsStream()
-            time.sleep(60 * 5)
-    else:
-        bbox = FindBBox(city)
+    cities = conf["city"]
+    for c in cities:
+        bbox = FindBBox(c)
         worker = TweetsCrawler(apikey, apisecret, accesstoken, accesssecret,
                                bearer_token, conf["keywordsfile"][topic],
-                               [city, None, bbox], conf["server"],
+                               [c, None, bbox], conf["server"],
                                conf["database"][topic])
         worker.GetTweetsStream()
+
+
+def gettweetsuburb(topic, city, conf, account_num):
+    apikey = conf["auth"][str(account_num)]["apikey"]
+    apisecret = conf["auth"][str(account_num)]["apisecret"]
+    accesstoken = conf["auth"][str(account_num)]["accesstoken"]
+    accesssecret = conf["auth"][str(account_num)]["accesssecret"]
+    bearer_token = conf["auth"][str(account_num)]["bearer_token"]
+    suburbs = conf["suburb"]
+    for suburb in suburbs:
+        name = suburb + ", " + city
+        try:
+            bbox = FindBBox(name)
+        except BaseException as e:
+            print("cannot find bbox: ", e)
+        worker = TweetsCrawler(apikey, apisecret, accesstoken, accesssecret,
+                               bearer_token, conf["keywordsfile"][topic],
+                               [city, suburb, bbox], conf["server"],
+                               conf["database"][topic])
+        worker.GetTweetsStream()
+        time.sleep(60 * 5)
 
 
 if __name__ == "__main__":
@@ -57,22 +73,14 @@ if __name__ == "__main__":
     f = open(configfilepath, 'r')
     conf = json.load(f)  # dict
     if RANK == 0:
-        with ThreadPoolExecutor(max_workers=2) as pool:
+        with ThreadPoolExecutor(max_workers=1) as pool:
             try:
-                pool.submit(gettweetsstream, topic1, conf["city"][0], conf, 1,
-                            True)
-                pool.submit(gettweetsstream, topic2, conf["city"][1], conf, 1,
-                            False)
+                pool.submit(gettweetsstream, topic1, conf, 1, True)
             except BaseException as e:
                 print("Error: cannot start threads")
     else:
-        with ThreadPoolExecutor(max_workers=2) as pool:
+        with ThreadPoolExecutor(max_workers=1) as pool:
             try:
-                pool.submit(gettweetsstream, topic2, conf["city"][2], conf, 2,
-                            False)
-                pool.submit(gettweetsstream, topic2, conf["city"][3], conf, 2,
-                            False)
-                pool.submit(gettweetsstream, topic2, conf["city"][0], conf, 2,
-                            False)
+                pool.submit(gettweetsstream, topic2, conf, 2, False)
             except BaseException as e:
                 print("Error: cannot start threads")
