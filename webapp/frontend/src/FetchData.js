645adead93caf936da36a2b1a1f5752550e308c7
shengyuj111 <Shengyuj@student.unimelb.edu.au>
2022-05-13 16:36:23 +1000

    Update FetchData.js

--- webapp/frontend/src/FetchData.js
+++ webapp/frontend/src/FetchData.js
@@ -15,7 +15,7 @@ export const request_tweets_proportion = (dates) => {
 }
 
 export const request_daily_Analysis = (dates) => {
-  return fetch(`/daily_Analysis?from_date=${encodeURIComponent(dates.from_date)}&to_date=${encodeURIComponent(dates.to_date)}&suburb=Melbourne,Carlton,Docklands,East Melbourne,Kensington,North Melbourne,Parkville,Port Melbourne,Southbank,South Yarra,West Melbourne&lan=English,French,Japanese,Indonesian,Thai,Portuguese`, {
+  return fetch(`/daily_Analysis?from_date=${encodeURIComponent(dates.from_date)}&to_date=${encodeURIComponent(dates.to_date)}&suburb=Melbourne,Carlton,Docklands,East Melbourne,Kensington,North Melbourne,Parkville,Port Melbourne,Southbank,South Yarra,West Melbourne&lan=en,in`, {
     method: 'GET', 
   }).then(response => {
     return response.json()
